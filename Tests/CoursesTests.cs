f8fd901a5b4d8a836a2c6036d03d124cd0163303
Chad Durkin and Kaz Matthews <chad+kaz@>
2017-02-28 09:00:10 -0800

    add initial files and folders.

---
+++ Tests/CoursesTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Registrar
+{
+    public class CourseTest : IDisposable
+    {
+        public CourseTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=registrar_test;Integrated Security=SSPI;";
+        }
+
+        [Fact]
+        public void Test_CitiesEmptyAtFirst()
+        {
+            //Arrange, Act
+            int result = Course.GetAll().Count;
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ Tests/StudentsTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Registrar
+{
+    public class StudentTest : IDisposable
+    {
+        public StudentTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=registrar_test;Integrated Security=SSPI;";
+        }
+
+        [Fact]
+        public void Test_CitiesEmptyAtFirst()
+        {
+            //Arrange, Act
+            int result = Student.GetAll().Count;
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+    }
+}
